a3e357334d7151a9d1e980b5f90e4ab1ceb4785b
Michael VanderMyde <mvandermyde4231@mail.kvcc.edu>
2024-04-15 13:34:00 -0400

    Use the package manager to connect to a Car Model Database and create a Controller for the Cars.

---
+++ cis237-inclass-6/Models/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace cis237_inclass_6.Models;
+
+public partial class Car
+{
+    [Key]
+    [Column("id")]
+    [StringLength(50)]
+    public string Id { get; set; }
+
+    [Required]
+    [Column("year")]
+    [StringLength(50)]
+    public string Year { get; set; }
+
+    [Required]
+    [Column("make")]
+    [StringLength(50)]
+    public string Make { get; set; }
+
+    [Required]
+    [Column("model")]
+    [StringLength(50)]
+    public string Model { get; set; }
+
+    [Required]
+    [Column("type")]
+    [StringLength(50)]
+    public string Type { get; set; }
+
+    [Column("horsepower")]
+    public int Horsepower { get; set; }
+
+    [Column("cylinders")]
+    public int Cylinders { get; set; }
+}
